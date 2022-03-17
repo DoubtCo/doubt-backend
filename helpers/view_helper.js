018210bb802084f026bcb91f997a08a2cfe19d11
devpatel <devpatel8907@gmail.com>
2022-03-17 16:12:46 +0530

    ip testing 2

--- helpers/view_helper.js
+++ helpers/view_helper.js
@@ -1,40 +1,61 @@
 const Question = require("../models/question")
 const Solution = require("../models/solution")
-
 exports.addView = async (req, res, next) => {
     try {
         const ip = req.ip;
         if (req.query.type == 'question') {
+            let flag=false;
             let question = await Question.findById(req.params.id);
             for (var i = 0; i < question.ips.length; i++) {
                 if (question.ips[i].ip == ip) {
-                    question.ips[i].count = min(question.ips[i].count + 1, 3);
+                    question.ips[i].count++;
+                    if(question.ips[i].count>3)
+                    {
+                        question.ips[i].count=3;
+                    }
+                    else{
+                        question.views++;
+                    }
+                    flag=true;
                     break;
                 }
             }
-
-            question.ips.push({
-                ip: ip,
-                count: 1
-            });
-            question.views++;
-            question.save();
-
+            if(!flag)
+            {
+                question.ips.push({
+                    ip: ip,
+                    count: 1
+                });
+                question.views++;
+            }
+            await question.save();
         }
         else if (req.query.type == 'solution') {
+            let flag=false;
             let solution = await Solution.findById(req.params.id);
             for (var i = 0; i < solution.ips.length; i++) {
                 if (solution.ips[i].ip == ip) {
-                    solution.ips[i].count = min(solution.ips[i].count + 1, 3);
+                    solution.ips[i].count++;
+                    if(solution.ips[i].count>3)
+                    {
+                        solution.ips[i].count=3;
+                    }
+                    else{
+                        solution.views++;
+                    }
+                    flag=true;
                     break;
                 }
             }
-            solution.ips.push({
-                ip: ip,
-                count: 1
-            });
-            solution.views++;
-            solution.save();
+            if(!flag)
+            {
+                solution.ips.push({
+                    ip: ip,
+                    count: 1
+                });
+                solution.views++;
+            }
+            await solution.save();
         }
         next();
     }
